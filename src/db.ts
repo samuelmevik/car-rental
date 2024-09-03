b720e8d1cb493d8c3ce6f3aba28df3a45df58837
samuelmevik <samuel.mevik@outlook.com>
2024-09-03 15:11:14 +0200

    Add: Car data

---
+++ src/db.ts
@@ -0,0 +1,64 @@
+const cars = [
+  {
+    name: "Audi A1 S-Line",
+    model: "Audi",
+    year: "2012",
+    doors:"4/5",
+    ac: "Yes",
+    transmission: "Manual",
+    price: "50",
+  },
+  {
+    name: "BMW 3 Series",
+    model: "BMW",
+    year: "2015",
+    doors:"4/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    price: "60",
+  },
+  {
+    name: "Chevrolet Camaro",
+    model: "Chevrolet",
+    year: "2017",
+    doors:"2/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    price: "70",
+  },
+  {
+    name: "Dodge Challenger",
+    model: "Dodge",
+    year: "2019",
+    doors:"2/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    price: "80",
+  },
+  {
+    name: "Ford Mustang",
+    model: "Ford",
+    year: "2018",
+    doors:"2/5",
+    ac: "Yes",
+    transmission: "Automatic",
+    price: "90",
+  }
+]
+
+
+const pickupLocations = [
+  "Airport",
+  "City",
+  "Hotel",
+  "Suburb",
+]
+
+const dropofLocations = [
+  "Airport",
+  "City",
+  "Hotel",
+  "Suburb",
+]
+
+export { cars, pickupLocations, dropofLocations }
