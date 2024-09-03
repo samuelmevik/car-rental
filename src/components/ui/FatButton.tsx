a07528d1d3e62db2fba6c67f6776f6727d5d6fd0
samuelmevik <samuel.mevik@outlook.com>
2024-09-03 15:12:04 +0200

    Misc: Use CSS Variables

--- src/components/ui/FatButton.tsx
+++ src/components/ui/FatButton.tsx
@@ -1,25 +1,15 @@
 type FatButtonProps = {
   text: string;
   hover?: boolean;
-  bgColor?: string;
-  borderColor?: string;
-  textColor?: string;
-  hoverBgColor?: string;
-  hoverTextColor?: string;
 }
 
 function FatButton({
   text,
   hover = false,
-  bgColor = "bg-orange-500",
-  borderColor = "border-orange-500",
-  textColor = "text-white",
-  hoverBgColor = "bg-transparent",
-  hoverTextColor = "text-black",
 }: FatButtonProps) {
   return (
     <button
-      className={`${bgColor} ${borderColor} ${textColor} py-2 px-6 border-2 duration-500 ${hover && `hover:${hoverBgColor} hover:${hoverTextColor}`}`}
+      className={`bg-primary border-primary py-2 px-6 border-2 duration-500 ${hover && `hover:bg-transparent hover:text-black`}`}
     >
       {text}
     </button>
