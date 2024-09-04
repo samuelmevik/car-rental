6b3450098f76f2f0c526d7c198d4c710248ab7bf
samuelmevik <samuel.mevik@outlook.com>
2024-09-04 17:45:30 +0200

    Misc: Background color removed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { MdPhoneInTalk } from "react-icons/md";
 function Footer() {
   return (
     <footer className="max-h-fit h-56 mt-auto">
-      <div className="h-full grid grid-cols-2 sm:grid-cols-4 gap-1 bg-secondary *:p-2 *:bg-white text-center">
+      <div className="h-full grid grid-cols-2 sm:grid-cols-4 gap-1 *:p-2 text-center">
       <div className="flex flex-col gap-2">
           <h2>Car Rental</h2>
           <p className="text-balance">We offers a big range of vehicles for all your driving needs. We have the perfect car to meet your needs.</p>
