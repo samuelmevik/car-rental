4686614ac9b0760692780af8aede1897fdd73e2a
samuelmevik <samuel.mevik@outlook.com>
2024-09-04 17:38:01 +0200

    Misc: new blue car

--- src/pages/home-page/components/Header.tsx
+++ src/pages/home-page/components/Header.tsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="h-[97.5svh]">
       <div className="h-full grid items-center">
         <div className="flex relative">
-          <div className="min-w-10 z-10 sm:max-w-lg grid text-center shadow-md p-4">
+          <div className="min-w-10 z-10 sm:max-w-lg grid text-center shadow-md p-4 bg-white opacity-90">
             <h2>Plan your trip now</h2>
             <h1>Rent <span className="text-primary">&</span> Save Big!</h1>
             <p className="text-balance">Drive your dream car today! Unbeatable rates, unlimited miles, flexible pick-up, and endless possibilities await you</p>
@@ -15,8 +15,8 @@ function Header() {
               <PrimaryButton text="Learn More" hover />
             </div>
           </div>
-          <div className="hidden z-0 sm:block absolute top-0 right-0 w-[calc(max(60vmin,600px))]">
-            <img className="transform aspect-video scale-x-[-1] object-cover" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.pngall.com%2Fwp-content%2Fuploads%2F2016%2F07%2FCar-PNG.png&f=1&nofb=1&ipt=ec49949521a8300cdb8d01d07811526ee8d65fcdb622ab5708e60c3b2627a697&ipo=images" alt="Car" />
+          <div className="hidden z-0 sm:block absolute top-0 right-0 w-[calc(max(60vmin,600px))] translate-y-[4rem]">
+            <img className="transform aspect-video object-cover" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpurepng.com%2Fpublic%2Fuploads%2Flarge%2Fpurepng.com-blue-jaguar-f-pace-side-view-carcarvehicletransportjaguar-9615246537762i7ef.png&f=1&nofb=1&ipt=0a563c93dc93c83833a2fb0dd9ee975287385abe8d84117aee6e8a2ad98db41e&ipo=images" alt="Car" />
           </div>
         </div>
       </div>
