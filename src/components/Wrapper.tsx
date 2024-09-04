0d180e743f3a30f9471d5c3a827bbf799c25786e
samuelmevik <samuel.mevik@outlook.com>
2024-09-04 17:33:27 +0200

    Misc: Changed padding

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,7 +2,7 @@ import { twMerge } from "tailwind-merge"
 
 function Wrapper({ children, className }: { children: React.ReactNode, className?: string }) {
   return (
-    <div className={twMerge("size-full mx-auto max-w-7xl px-12 grid gap-12", className)}>
+    <div className={twMerge("size-full mx-auto max-w-7xl px-1 grid gap-12", className)}>
       {children}
     </div>
   )
