97ec27301c235ce53e43b94de47dd502826652e2
samuelmevik <samuel.mevik@outlook.com>
2024-09-04 10:31:11 +0200

    Misc: Balanced Text

--- src/components/OrderSection.tsx
+++ src/components/OrderSection.tsx
@@ -12,21 +12,21 @@ function OrderGuide() {
             <IoCarSport className="size-28 text-primary" />
           </div>
           <h2>Select Car</h2>
-          <p>We provide a wide selection of vehicles to suit all your driving requirements. Whatever you need, we have the ideal car for you</p>
+          <p className="text-balance">We provide a wide selection of vehicles to suit all your driving requirements. Whatever you need, we have the ideal car for you</p>
         </div>
         <div className="grid">
           <div style={{ borderRadius: "75% 25% 41% 59% / 66% 51% 49% 34%" }} className="size-40 justify-self-center bg-secondary grid place-items-center">
             <IoPerson className="size-28 text-primary" />
           </div>
           <h2>Contact Operator</h2>
-          <p>Our experienced and approachable team is always prepared to assist with any questions or concerns.</p>
+          <p className="text-balance">Our experienced and approachable team is always prepared to assist with any questions or concerns.</p>
         </div>
         <div className="grid">
           <div style={{ borderRadius: "23% 77% 68% 32% / 66% 33% 67% 34% " }} className="size-40 justify-self-center bg-secondary grid place-items-center">
             <GiCarKey className="size-28 text-primary" />
           </div>
           <h2>Let's Drive</h2>
-          <p>No matter where your journey takes you, our extensive selection of cars has you covered.</p>
+          <p className="text-balance">No matter where your journey takes you, our extensive selection of cars has you covered.</p>
         </div>
       </div>
     </div>
