33e26dca9b2b853e37b72cfaa9d09ef383e0fc33
samuelmevik <samuel.mevik@outlook.com>
2024-09-04 16:31:13 +0200

    Misc: Navbar effect

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar className="h-16" />
+      <div className="fixed -z-50 top-0 right-0 h-2 w-full bg-primary" />
       <Wrapper className="pt-16 min-h-svh">
         {children}
         <Footer />
