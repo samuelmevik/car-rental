b3aeab6748fea0887496c898fd01cdc2a11edc48
samuelmevik <samuel.mevik@outlook.com>
2024-09-05 14:52:19 +0200

    Misc: Solved overflow issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ import Wrapper from "./Wrapper";
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div className="overflow-x-hidden">
+      <div className="overflow-hidden">
         <Navbar className="h-16" />
         <div className="fixed z-20 top-0 right-0 h-2 w-full bg-primary" />
         <Wrapper className="pt-16 min-h-svh">
