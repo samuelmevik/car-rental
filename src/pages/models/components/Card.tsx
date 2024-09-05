05386a94e76ae76137b81d86b4ca5d05f16aa236
samuelmevik <samuel.mevik@outlook.com>
2024-09-05 15:41:40 +0200

    Misc: Improved mobile version

--- src/pages/models/components/Card.tsx
+++ src/pages/models/components/Card.tsx
@@ -1,6 +1,4 @@
-import { FaCarSide } from "react-icons/fa";
 import { Car } from "../../../db";
-import { TbManualGearbox } from "react-icons/tb";
 import PrimaryButton from "../../../components/ui/PrimaryButton";
 
 function Card(car: Car) {
@@ -9,6 +7,30 @@ function Card(car: Car) {
       <div className="grid grid-rows-2">
         <img src={car.img} alt={car.name} className="object-cover object-center size-full" />
         <div className="grid px-4 pb-4">
+          <div className="grid place-items-center">
+            <h3>{[...Array(5)].map((_, index) => index < car.stars ? (<span>★</span>) : (<span>☆</span>))}</h3>
+          </div>
+          <div className="flex justify-between">
+            <h3>{car.name}</h3>
+            <h3>${car.price}<span className="text-primary">/</span><span className="text-base">per day</span></h3>
+          </div>
+          <div className="flex justify-between">
+            <p>🚘 {car.model}</p>
+            <p>{car.doors} doors</p>
+          </div>
+          <div className="flex justify-between">
+            <p>AC: {car.ac}</p>
+            <p>Type: {car.transmission}</p>
+          </div>
+          <PrimaryButton text="Rent Now" hover />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+/*
+
           <div className="flex justify-center">
             <h3>
               {[...Array(5)].map((_, index) => index < car.stars ? (<span>★</span>) : (<span>☆</span>))}
@@ -32,10 +54,7 @@ function Card(car: Car) {
             <span className="inline-flex gap-1 items-center" ><TbManualGearbox /><p>{car.transmission}</p></span>
           </div>
           <PrimaryButton text="Rent Now" hover />
-        </div>
-      </div>
-    </div>
-  );
-}
+
+*/
 
 export default Card;
