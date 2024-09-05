1d3ba7a22dd90fba7b5f6827a0bbaa00871a6f0d
samuelmevik <samuel.mevik@outlook.com>
2024-09-05 14:53:21 +0200

    Misc: Cleanup

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,16 +4,14 @@ import Wrapper from "./Wrapper";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <div className="overflow-hidden">
-        <Navbar className="h-16" />
-        <div className="fixed z-20 top-0 right-0 h-2 w-full bg-primary" />
-        <Wrapper className="pt-16 min-h-svh">
-          {children}
-          <Foot />
-        </Wrapper>
-      </div>
-    </>
+    <div className="overflow-hidden">
+      <Navbar className="h-16" />
+      <div className="fixed z-20 top-0 right-0 h-2 w-full bg-primary" />
+      <Wrapper className="pt-16 min-h-svh">
+        {children}
+        <Foot />
+      </Wrapper>
+    </div>
   );
 }
 
